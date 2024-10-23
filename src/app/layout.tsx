0d73d6ecaa81312987f0e20d5a28b6d34041808a
Daya3611 <dayanand.gawade@vsit.edu.in>
2024-10-23 18:50:12 +0530

    server change1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body  className={bricolage_grotesque.className}>
-      <Header/>
+      {/* <Header/>
         {children}
-      <Footer/>
+      <Footer/> */}
+        <h1 className="text-center">"This site is temporarily unavailable. Please check back later."</h1>
       </body>
     </html>
   );
