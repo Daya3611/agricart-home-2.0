f7b0c37e3e1b609110219b50601dc01572404633
Daya3611 <dayanand.gawade@vsit.edu.in>
2024-11-14 20:38:21 +0530

    SEO keyeord add

--- src/app/_components/Seo.tsx
+++ src/app/_components/Seo.tsx
@@ -23,6 +23,96 @@ const keywords = [
     {
         word:'agricart maharashtra seed bank'
     },
+    {
+        word:'Sindhudurg'
+    },
+    {
+        word:'red rice'
+    },
+    {
+        word:'BAIF'
+    },
+    {
+        word:'Seed Bank'
+    },
+    {
+        word:'Digas'
+    },
+    {
+        word:'Humarmala'
+    },
+    {
+        word:'Ranbambuli'
+    },
+    {
+        word:'Santosh Gawade'
+    },
+    {
+        word:'Black rice'
+    },
+    {
+        word:'Horsegram'
+    },
+    {
+        word:'Kulith'
+    },
+    {
+        word:'Baba Prasad'
+    },
+    {
+        word:'Traditional rice seeds'
+    },
+    {
+        word:'UNDP'
+    },
+    {
+        word:'SGP'
+    },
+    {
+        word:'Sanjay Patil'
+    },
+    {
+        word:'Sachin Chorge'
+    },
+    {
+        word:'Kudal'
+    },
+    {
+        word:'Sandeep Dhawale'
+    },
+    {
+        word:'AgroBiodiversity'
+    },
+    {
+        word:'Vasundhara'
+    },
+    {
+        word:'NABARD'
+    },
+    {
+        word:'LUPIN FOUNDATION'
+    },
+    {
+        word:'Dayanand Gawade'
+    },
+    {
+        word:'Humarmala'
+    },
+    {
+        word:'Anav'
+    },
+    {
+        word:'Hirlok'
+    },
+    {
+        word:'Gawathi Bazar'
+    },
+    {
+        word:'FPO'
+    },
+    
+    
+
 ]
 export default function Seo() {
   return (
