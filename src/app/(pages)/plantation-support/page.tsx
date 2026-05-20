05f452ecab275dd39fc9b26effbd2e40d963fd28
Daya3611 <hi@dayanandgawade.in>
2026-05-20 13:33:19 +0530

    add: new page add:

---
+++ src/app/(pages)/plantation-support/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+page.propTypes = {
+    
+};
+
+function page(props :any) {
+    return (
+        <div className='mt-[90px]'>
+            sample page
+        </div>
+    );
+}
+
+export default page;
